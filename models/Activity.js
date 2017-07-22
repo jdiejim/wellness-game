872b4d2f3c685cba5bcf5f381f5a82534c955193
Juan Diego Jimenez <jdiejim@gmail.com>
2017-07-22 15:53:06 -0600

    add create activity function in activities controller

--- models/Activity.js
+++ models/Activity.js
@@ -1,20 +1,11 @@
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('../knexfile')[environment];
-// const db = require('knex')(configuration);
-//
-//
-// // const userTemplate = ({ id, first_name, last_name, email, avatar }) => ({ id, first_name, last_name, email, avatar });
-//
-// // const getUser = ({ email, password: raw }) => {
-// //   const password = hash(raw);
-// //
-// //   return db('users').where({ email, password }).select();
-// // }
-//
-// const createUser = (body) => {
-//   body.password = hash(body.password);
-//
-//   return db('users').insert(body, ['id', 'first_name', 'last_name', 'email', 'avatar']);
-// }
-//
-// module.exports = { userTemplate, createUser, getUser };
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const db = require('knex')(configuration);
+
+const createActivity = (activities) => {
+  const promises = activities.map(activity => db('activities').insert(activity, ['user_id', 'description', 'type', 'points', 'status']));
+
+  return Promise.all(promises);
+}
+
+module.exports = { createActivity };
