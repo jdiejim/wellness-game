cd0371498046f3bf5fb54ee403d9d9db98437b55
Juan Diego Jimenez <jdiejim@gmail.com>
2017-07-22 14:03:07 -0600

    cache users in dashboard if already in state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,10 +4,26 @@ import { Banner } from '../elements';
 import './styles/Dashboard.css';
 
 class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
   componentDidMount() {
     const { fetchUsers } = this.props;
+    const { users } = this.state;
 
-    fetchUsers();
+    if (!users.length) {
+      fetchUsers();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.users.length !== nextProps.users.length) {
+      this.setState({ users: nextProps.useres });
+    }
   }
 
   render() {
