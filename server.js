5fb08930bed4d22624c1412b3ead1d61e596aa27
Juan Diego Jimenez <jdiejim@gmail.com>
2017-08-18 10:46:59 -0600

    fix error in deployment

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname, './public')));
 app.use('/api/v1', router);
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './public', 'index.html')));
+app.use(express.static(path.resolve(__dirname, './build')));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './build', 'index.html')));
 
 app.listen(app.get('port'), () => console.log(`${app.locals.title} is running on ${app.get('port')}`));
